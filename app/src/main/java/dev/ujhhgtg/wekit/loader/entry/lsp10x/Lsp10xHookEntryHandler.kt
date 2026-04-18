c4c7b4835b0feb047e312411864aa406e88dc42d
Fey Xie <feyxiexzf@gmail.com>
2026-04-18 12:09:04 +0800

    remove libxposed

--- app/src/main/java/dev/ujhhgtg/wekit/loader/entry/lsp10x/Lsp10xHookEntryHandler.kt
+++
@@ -1,9 +0,0 @@
-package dev.ujhhgtg.wekit.loader.entry.lsp10x
-
-import androidx.annotation.Keep
-import io.github.libxposed.api.XposedModuleInterface.PackageLoadedParam
-
-@Keep
-interface Lsp10xHookEntryHandler {
-    fun onPackageLoaded(param: PackageLoadedParam)
-}
